ec15b982163d0ab1c5729541ed088767bf35e49d
Axel Ogereau-Peltier <49279289+axel-op@users.noreply.github.com>
2020-10-11 01:29:14 +0200

    Accélère le temps de pré-traitement

--- src/graph.js
+++ src/graph.js
@@ -8,7 +8,7 @@ function indexById(objects, id, allow_duplicates = false) {
     return objects.reduce((map, o) => {
         if (map[o[id]]) {
             if (allow_duplicates) map[o[id]].push(o);
-            else throw new Error('Duplicate key');
+            else throw new Error(`Duplicate key: ${o[id]}`);
         }
         else map[o[id]] = allow_duplicates ? [o] : o;
         return map;
@@ -30,18 +30,26 @@ function addField(from, index, field, id, map) {
 
 async function buildGraph() {
     console.log('Building graph...');
-    let medicaments = await data.getProperties('CIS_bdpm');
+    console.time('Graph built');
+    const files = {
+        medicaments: 'CIS_bdpm', 
+        presentations: 'CIS_CIP_bdpm',
+        conditions: 'CIS_CPD_bdpm',
+        substances: 'CIS_COMPO_bdpm',
+    };
+    Object.keys(files).forEach(k => files[k] = data.getProperties(files[k]));
+    let medicaments = await files.medicaments;
     medicaments.forEach(m => {
         m['presentations'] = [];
         m['conditions_prescription'] = [];
         m['substances'] = [];
     });
     medicaments = indexById(medicaments, 'code_CIS');
-    const presentations = await data.getProperties('CIS_CIP_bdpm');
+    let presentations = await files.presentations;
     medicaments = addField(presentations, medicaments, 'presentations', 'code_CIS');
-    const conditions = await data.getProperties('CIS_CPD_bdpm');
+    let conditions = await files.conditions;
     medicaments = addField(conditions, medicaments, 'conditions_prescription', 'code_CIS', o => o['conditions_prescription']);
-    let substances = await data.getProperties('CIS_COMPO_bdpm');
+    let substances = await files.substances;
     for (let substance of substances) substance['substance'] = {
         'code_substance': substance['code_substance'],
         'denomination': substance['denomination']
@@ -50,13 +58,13 @@ async function buildGraph() {
     substances = indexById(substances, 'code_substance', true);
     Object.keys(substances).forEach((c, _) => substances[c] = substances[c][0]);
     const graph = {
+        'substances': substances,
         'medicaments': medicaments,
         'presentations': {
             'CIP7': indexById(presentations, 'code_CIP7'),
             'CIP13': indexById(presentations, 'code_CIP13'),
         },
-        'substances': substances,
     };
-    console.log('Graph built');
+    console.timeEnd('Graph built');
     return graph;
 }
